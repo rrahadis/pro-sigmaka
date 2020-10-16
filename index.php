e6b628632150ef184d1abaf227f68ceccaddb0e1
rrahadis <rahadi.solehudin@gmail.com>
2020-10-16 18:55:20 +0700

    update paggination

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include 'koneksi.php'; ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -26,42 +27,63 @@
 </nav>
 
 <div class="container">
-    <br>
-    <br>
-    <h2>List Data</h2>
-    <table class="table">
-  <thead class="thead-dark">
-    <tr>
-        <tr><th>NO</th><th>NAMA</th><th>ALAMAT</th><th>CEK</th><th>UBAH</th><th>HAPUS</th>
-    </tr>
-  </thead>
-  <tbody>
-  
-	<?php
-    include 'koneksi.php';
-    $mahasiswa = mysqli_query($koneksi, "SELECT * from data_mahasiswa");
-    $no=1;
-    foreach($mahasiswa as $row){
-        echo "<tr>";
-        echo "<td>" . $no . "</td>";
-        echo "<td>" . $row['nama'] . "</td>";
-        echo "<td>" . $row['alamat'] . "</td>";
-        echo "<td><a href='detail.php?id=" . $row['id'] . "'>Detail</a></td>";
-        echo "<td><a href='form-update.php?id=" . $row['id'] . "'>Edit</a></td>";
-        echo "<td><a href='delete.php?id=" . $row['id'] . "'>Hapus</a></td>";
-        echo "</tr>";
-      
-        $no++;
-    }
-    ?>
-  </tbody>
-</table>
 
-    
-    
-
+		<br>		
+		<table class="table table-bordered">
+			<thead class="thead-dark">
+				<tr>
+        <th>NO</th><th>NAMA</th><th>ALAMAT</th><th>CEK</th><th>UBAH</th><th>HAPUS</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+				$batas = 3;
+				$halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
+				$halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;	
  
-</div>
+				$previous = $halaman - 1;
+				$next = $halaman + 1;
+				
+				$data = mysqli_query($koneksi,"select * from data_mahasiswa");
+				$jumlah_data = mysqli_num_rows($data);
+				$total_halaman = ceil($jumlah_data / $batas);
+ 
+				$data_mahasiswa = mysqli_query($koneksi,"select * from data_mahasiswa limit $halaman_awal, $batas");
+				$nomor = $halaman_awal+1;
+				while($d = mysqli_fetch_array($data_mahasiswa)){
+					?>
+					<tr>
+						<td><?php echo $nomor++; ?></td>
+              <td class="align-middle"><?php echo $d['nama']; ?></td>
+              <td class="align-middle"><?php echo $d['alamat']; ?></td>
+             <?php echo "<td><a href='detail.php?id=" . $d['id'] . "'>Detail</a></td>";?>
+              <?php echo "<td><a href='form-update.php?id=" . $d['id'] . "'>Edit</a></td>"; ?>
+              <?php echo "<td><a href='delete.php?id=" . $d['id'] . "'>Hapus</a></td>"; ?>
+					</tr>
+					<?php
+				}
+				?>
+			</tbody>
+		</table>
+		<nav>
+			<ul class="pagination justify-content-center">
+				<li class="page-item">
+					<a class="page-link" <?php if($halaman > 1){ echo "href='?halaman= $previous'"; } ?>>Previous</a>
+				</li>
+				<?php 
+				for($x=1;$x<=$total_halaman;$x++){
+					?> 
+					<li class="page-item"><a class="page-link" href="?halaman=<?php echo $x ?>"><?php echo $x; ?></a></li>
+					<?php
+				}
+				?>				
+				<li class="page-item">
+					<a  class="page-link" <?php if($halaman < $total_halaman) { echo "href='?halaman=$next'"; } ?>>Next</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+
 
 
 	
@@ -79,3 +101,4 @@
     -->
   </body>
 </html>
+
